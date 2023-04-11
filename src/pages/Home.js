3357dc1940e11a3ee897e59f1b0ec3fb6acdfaeb
MathisPastreNelson <shiray@hotmail.fr>
2023-04-11 15:51:19 +0200

    ajout composant AlbumsThisMonth pour futur liaison avec API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Header from "../components/Header";
+import AlbumsThisMonth from "../components/AlbumsThisMonth";
 import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <AlbumsThisMonth />
       <Footer />
     </div>
   );
