999613f8af3312af1c9b88a6227c916d78ebb482
MathisPastreNelson <shiray@hotmail.fr>
2023-04-13 21:59:34 +0200

    animation du disque OK

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { data } from "../assets/data"; // importer une liste de projets
+import vynil from "../assets/vynil.png"; // importer le fichier SVG
 
 const Artist = () => {
   console.log({ data });
@@ -7,17 +8,27 @@ const Artist = () => {
     <div className="mainContainer">
       {data.map((item, index) => (
         <div
+          // Alternance de classe 1 fois sur 2
           className={
             index % 2 === 0
               ? "albumBox albumBox--odd"
               : "albumBox albumBox--even"
           }
           key={item.id}>
-          <img
-            className="albumImage"
-            src={item.album && item.album.cover_medium}
-            alt={item.title_short}
-          />
+          <div className="albumImageWrapper">
+            <img
+              className="albumImage"
+              src={item.album && item.album.cover_medium}
+              alt={item.title_short}
+            />
+            <img
+              className={
+                index % 2 === 0 ? "discFillImage" : "discFillImageReverse"
+              }
+              src={vynil}
+              alt="Disc fill"
+            />
+          </div>
           <div className="albumDescribeBox">
             <h3 className="albumTitle">{item.title}</h3>
             <audio className="albumAudioPreview" controls>
